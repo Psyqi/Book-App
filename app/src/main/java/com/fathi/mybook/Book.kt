eef9dbf7c2e3f9959179e1e6e16adfc622776bce
Psyqi <78408078+Psyqi@users.noreply.github.com>
2022-08-24 07:18:01 +0700

    Update Book.kt

--- app/src/main/java/com/fathi/mybook/Book.kt
+++ app/src/main/java/com/fathi/mybook/Book.kt
@@ -1,8 +1,12 @@
-package com.fathi.mybook
+package com.fathi.recyclerview
 
 data class Book(
 
     var title: String = "",
     var detail: String = "",
+    var summary: String = "",
+    var write: String = "",
+    var create: String = "",
+    var image: Int = 0,
     var psd: Int = 0
 )
